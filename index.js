479148ebafb7c5015113c6e4498ca06eef8a9959
Allan Monzon <greengamer.am@gmail.com>
2022-11-30 18:19:36 -0500

    commit tree 2f295073950691cac3ae0b7dd01736e25ac0b508

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"},
+    { year: "2007", result: "N/A"},
+    { year: "2006", result: "N/A"},
+    { year: "2005", result: "N/A"},
+    { year: "2004", result: "N/A"},
+    { year: "2003", result: "N/A"},
+    { year: "2002", result: "N/A"},
+    { year: "2001", result: "N/A"},
+    { year: "2000", result: "N/A"},
+    
+  ]
+
+function superbowlWin(record){
+  let wear = record.find(object => console.log(object.result)>1 )
+  let wearr = record.find(object => console.log(object.year)> 4 )
+  
+     if(wear){
+      let wear = record.find(object => console.log(object.year) > '2015')
+       return wear
+     }
+     
+else if (wearr) {
+return wearr
+}
+
+
+}
